1385580e0403519ca04374f8d41162e203975e3f
martincallesen <mail@martincallesen.dk>
2021-07-08 10:04:06 +0200

    generate random name

--- src/main/groovy/com/github/examples/DataGenerator.groovy
+++ src/main/groovy/com/github/examples/DataGenerator.groovy
@@ -5,8 +5,17 @@ import org.apache.commons.lang3.RandomStringUtils
 
 class DataGenerator {
     static Account generateRandomAccount() {
+        new Account(name: randomName(), emailAddress: randomEmail(), accountSettings: [])
+    }
+
+    private static String randomName() {
+        def name = RandomStringUtils.random(25, true, false)
+        def surName = RandomStringUtils.random(25, true, false)
+        "$name $surName"
+    }
+
+    private static def randomEmail() {
         def randomString = RandomStringUtils.random(50, true, true)
-        def email = "$randomString@thing.dk"
-        new Account(name: 'something', emailAddress: email, accountSettings: [])
+        "$randomString@thing.dk"
     }
 }
